9afda18720c7091ebc59c78fa486eecd745b5e43
aykwon <pj.aykwon@kalmate.net>
2024-08-27 18:59:44 +0900

    SPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,15 +17,23 @@ public class JpaMain {
 
         try{
 
+            /* Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);*/
+
             Member findMember =  em.find(Member.class, 1L);
             findMember.setName("HelloJPA");
 
             System.out.println("findMember:"+findMember.getId());
             System.out.println("findMember:"+findMember.getName());
-           /* Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
-            em.persist(member);*/
+
+            // Member 객체를 대상으로 (Member는 테이블이 아니다.)
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for(Member member: result){
+                System.out.println("member:"+member.getName());
+            }
 
             tx.commit();
         }catch(Exception e){
