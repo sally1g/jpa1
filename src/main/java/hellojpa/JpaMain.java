238e2b6fc614eabf6eca2471d84cc46a5a0535a3
aykwon <pj.aykwon@kalmate.net>
2024-08-27 18:35:06 +0900

    first commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // transaction
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            em.persist(member);
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        //code
+
+        emf.close();
+    }
+}
